4fad2d6849a6276415529193f36833004bc87d0a
MikeEngerer <michael.engerer@hotmail.com>
2019-01-14 01:17:39 +0000

    username change now checks for same name and profanity, comments added elsewhere

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -34,11 +34,13 @@ wss.on('connection', (ws) => {
   const oneOrMore = wss._server._connections > 1 ? 
     `New user connected! There are now ${wss._server._connections} active users.` : 
     "Welcome! Its only you in here until others join.";
+  // data obj for system message and data handling pertaining to new connection
   const newConnection = {
     type: 'incomingNotification',
     content: oneOrMore,
     count: wss._server._connections,
     id: uuidv4(),
+    // assigns color from array of 4 at random, function at bottom
     userColor: assignColor()
   }
   console.log('Client connected, count:', wss._server._connections)
@@ -46,17 +48,17 @@ wss.on('connection', (ws) => {
 
   ws.on('message', (data) => {
     const parsedData = JSON.parse(data);
-    console.log(parsedData)
+    // different obj are broadcasted depending on type property recieved from client, appropriate types set for msg render
     switch (parsedData.type) {
       case 'postNotification':
-        // data object for system message
+        // data object for system message pertaining to name change
         const notification = {
           type: 'incomingNotification',
           username: parsedData.name,
           count: wss._server._connections,
           content: `${parsedData.oldName} changed their name to ${parsedData.newName}`,
           id: uuidv4()
-        }
+        };
         wss.broadcast(JSON.stringify(notification));
         break;
       case 'postMessage':
@@ -68,50 +70,35 @@ wss.on('connection', (ws) => {
           username: parsedData.username,
           content: parsedData.content,
           userColor: parsedData.userColor
-        }
+        };
         console.log(`User ${messageData.username} said ${messageData.content}`);
         wss.broadcast(JSON.stringify(messageData));
-        break;
-      case 'postGif':
-        // data obj for GIF message (wip)
-        const displayImg = {
-          id: uuidv4(),
-          type: 'incomingGif',
-          count: wss._server._connections,
-          content: handleUrl(parsedData.content)
-        }
-        wss.broadcast(JSON.stringify(displayImg));
         break;
     } 
   })
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => {
-    console.log('Client disconnected', wss._server._connections)
+    console.log('Client disconnected', wss._server._connections);
+    // message displayed on user disconnect differs depending on active user count
     const oneOrMore = wss._server._connections > 1 ? 
       `There are now ${wss._server._connections} active users.` : 
-      "It's just you left in here. :("
+      "It's just you left in here. :(";
+    // data obj for disconnect system msg
     const disconnect = {
       type: 'incomingNotification',
       content: `A user has disconnected. ${oneOrMore}`,
       count: wss._server._connections,
       id: uuidv4()
-    }
-    wss.broadcast(JSON.stringify(disconnect))
+    };
+    wss.broadcast(JSON.stringify(disconnect));
   })
 
 });
 
-function handleUrl(url) {
-  console.log("recieved new gif:", url);
-  let content;
-  fetch(url)
-    .then(res => res.json())
-    .then(json => console.log(json))
-}
-
+// assigns color to new user which is recorded in state and later used for each message produced by this user
 function assignColor() {
- const colors = ['#50c8b4', '#2f4558', '#50c878', '#bdf8d1']
- const random = Math.floor(Math.random() * Math.floor(3))
- return colors[random]
+ const colors = ['#50c8b4', '#2f4558', '#50c878', '#bdf8d1'];
+ const random = Math.floor(Math.random() * Math.floor(3));
+ return colors[random];
 }
